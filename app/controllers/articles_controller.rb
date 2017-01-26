5b044545eece294dcbcca74a17ea2b3cf24cedc4
Michael Groff <bgmichael1400@gmail.com>
2017-01-26 20:27:19 +0000

    Adding authentication.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    
+    http_basic_authenticate_with name: "groff", password: "Avidblogger0", except: [:index, :show]
+    
     def index
         @articles = Article.all
     end
