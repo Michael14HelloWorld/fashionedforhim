fb63a27284ecca834ff86903cde9152edb239460
Michael Groff <bgmichael1400@gmail.com>
2017-01-26 21:07:25 +0000

    Clean up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
-  devise_for :admins
-  get 'login/index'
 
   get 'welcome/index'
   
   root 'welcome#index'
-  
-  resources :login
   
   resources :articles do
     resources :comments
