c2ffbbec5c3d273a8ea606a8517c670703e6d684
Michael Groff <bgmichael1400@gmail.com>
2017-03-18 02:36:29 +0000

    Disabling Devise sign_up.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #  :registerable, "Disabling any John Doe from creating an account."
          :recoverable, :trackable, :validatable
 end
