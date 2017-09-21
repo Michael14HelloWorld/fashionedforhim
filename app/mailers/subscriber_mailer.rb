1bbe9423db57089f56f8fd38e7c3811552bf3b57
Michael Groff <bgmichael1400@gmail.com>
2017-09-21 19:49:28 +0000

    Cleaning up.

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,2 +1,10 @@
 class SubscriberMailer < ApplicationMailer
+    default from: "fashionedforhim40@gmail.com"
+    layout 'mailer'
+  
+    def welcome_email(subscriber)
+      @subscriber = subscriber
+      @url  = 'http://fashionedforhim.com'
+      mail(to: @subscriber.email, subject: 'Thanks for Subscribing!')
+    end
 end
